cddf9373cfd16d64394da407674134020b5b6fb9
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-11-10 20:39:20 -0300

    Configurando Babel e Express

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000"));
