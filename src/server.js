55705984ec00458be3026ae0e3e0766d60590f04
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-11-11 20:40:52 -0300

    Separando execução de configuração e instalando Mocha,  Chai e Supertest

--- src/server.js
+++ src/server.js
@@ -1,9 +1,4 @@
-import express from "express";
+import app from "./app.js";
+const port = 3000;
 
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(3000, () => console.log("Example app listening on port 3000"));
+app.listen(port, () => console.log(`App running on port ${port}`));
