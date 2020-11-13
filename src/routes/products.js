408197ab3d48944585ec2484b88c4cac93231e9f
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-11-12 21:15:07 -0300

    Inicio dos controllers, usando Classe

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,9 @@
 const express = require("express");
+const ProductsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send([
-    {
-      name: "default product",
-      description: "product description",
-      price: 100,
-    },
-  ]);
-});
+const productsController = new ProductsController(); // Precisa criar a instancia
+router.get("/", (req, res) => productsController.get(req, res)); // Para usar aqui
 
 module.exports = router;
