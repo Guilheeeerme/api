7dd550b919e9393368b374686a42a2d071180002
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-12-28 16:00:43 -0300

    Controllers

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -11,6 +11,45 @@ class ProductsController {
       res.status(400).send(error.message);
     }
   }
+
+  async getById(req, res) {
+    const { id } = req.params;
+    try {
+      const products = await this.Product.find({ _id: id });
+      res.send(products);
+    } catch (error) {
+      res.status(500).send(new Error(error.message));
+    }
+  }
+
+  async create(req, res) {
+    const product = new this.Product(req.body);
+
+    try {
+      await product.save();
+      res.status(201).send(product);
+    } catch (error) {
+      res.status(422).send(error.message);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      await this.Product.updateOne({ _id: req.params.id }, req.body);
+      res.sendStatus(200);
+    } catch (error) {
+      res.status(422).send(error.message);
+    }
+  }
+
+  async remove(req, res) {
+    try {
+      await this.Product.deleteOne({ _id: req.params.id });
+      res.sendStatus(204);
+    } catch (error) {
+      res.status(422).send(error.message);
+    }
+  }
 }
 
 module.exports = ProductsController;
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,5 +6,9 @@ const productsController = new ProductsController(Product);
 const router = express.Router();
 
 router.get("/", (req, res) => productsController.get(req, res));
+router.get("/:id", (req, res) => productsController.getById(req, res));
+router.post("/", (req, res) => productsController.create(req, res));
+router.put("/", (req, res) => productsController.update(req, res));
+router.delete("/:id", (req, res) => productsController.remove(req, res));
 
 module.exports = router;
